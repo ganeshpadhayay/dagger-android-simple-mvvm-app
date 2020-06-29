04d85a729c5dae4b3dd3dcf4e5dc788da9243abb
Ganesh Padhayay <ganeshraj020794@gmail.com>
2020-06-29 12:12:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dagger2_practical/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dagger2_practical
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
