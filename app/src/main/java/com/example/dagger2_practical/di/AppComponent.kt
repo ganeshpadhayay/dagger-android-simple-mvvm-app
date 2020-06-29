e2a646c5e5e2daa9b1c38001ed373883153b3b08
Ganesh Padhayay <ganeshraj020794@gmail.com>
2020-06-29 13:06:20 +0530

    appcomponent setup added using the latest dagge AndoidInjector

---
+++ app/src/main/java/com/example/dagger2_practical/di/AppComponent.kt
@@ -0,0 +1,29 @@
+package com.example.dagger2_practical.di
+
+import android.app.Application
+import com.example.dagger2_practical.BaseApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+
+/***
+ * AndroidInjector is basically taking the DI to next level and setting up the client(BaseApplication) and Server(AppComponent) hierarchy
+ * In general, the components are servers and serve dependencies and activity/fragments are clients who consume these dependencies
+ * If we have SubComponents in the system then SubComponents can also act as clients
+ */
+@Component(modules = [AndroidSupportInjectionModule::class])
+interface AppComponent : AndroidInjector<BaseApplication> {
+
+    //we are overriding the Builder so that we can customize it
+    @Component.Builder
+    interface Builder {
+
+        //this is used to bind a particular instance of an object to a component at the time of component's creation
+        @BindsInstance
+        fun application(app: Application): Builder
+
+        fun build(): AppComponent
+    }
+
+}
